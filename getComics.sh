dd481ba5b5572fe084bddc03ef3130dbb90d5e21
Sean Scott <3287046+oraclesean@users.noreply.github.com>
2025-08-11 08:10:00 -0600

    Update getComics.sh
    Updated to handle revised asset formatting

--- getComics.sh
+++ getComics.sh
@@ -3,5 +3,6 @@
 path=~/MagicMirror/modules/MMM-ImageSlideshow/Images
 
  for comic in $(cat comic.lst)
-  do curl -s -o "${path}"/"${comic}".jpg $(curl -s https://www.gocomics.com/"${comic}" | sed -e "s|^.*\(https://featureassets.gocomics.com/assets/[A-Za-z0-9]*\)?.*$|\1\?optimizer=image\&width=1200\&quality=85|")
+  do curl -s -o "${path}"/"${comic}".jpg https://featureassets.gocomics.com/assets/"$(curl -s https://www.gocomics.com/"${comic}" \
+   | sed -e "s|^.*\\\"comicBlobName\\\\\":\\\\\"\([A-Za-z0-9]*\)\\\\\".*$|\1|")"?optimizer=image\&width=1200\&quality=85
 done
